7e39a88b3634bddb4bba5e1e6a8a42f1ab601025
Crowdbotics <team@crowdbotics.com>
2022-01-09 00:50:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "moonsong_coaching_32753",
+  url: "https://moonsong_coaching_32753.botics.co",
+  api: "https://moonsong_coaching_32753.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
